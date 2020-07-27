57be591d3ca2f5e347a345abde7ba3ae417af023
Steffen Kühne <stekhn@gmail.com>
2020-07-27 17:12:17 +0200

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    modules: ['node_modules']
+  },
+  entry: {
+    main: path.resolve('./src/map.js')
+  },
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'sass-loader',
+          options: {
+            implementation: require('sass'),
+            sourceMap: true
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+  devtool: '#cheap-source-map',
+  devServer: {
+    contentBase: path.resolve('./dist'),
+    port: 9000
+  }
+};
