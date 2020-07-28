966de3c13ff22990337c5942ce395fbd4e86d6e2
Steffen Kühne <stekhn@gmail.com>
2020-07-28 18:51:32 +0200

    Add animation control

--- src/map.js
+++ src/map.js
@@ -1,5 +1,5 @@
 import './map.scss';
-import { select, selectAll } from 'd3-selection';
+import { select } from 'd3-selection';
 import { geoPath, geoMercator } from 'd3-geo';
 import { scaleSqrt } from 'd3-scale';
 import 'd3-transition';
@@ -15,11 +15,15 @@ async function init() {
     .then(({ default: data }) => data);
   const [timelineData, metaData, geoData] = await Promise.all([timelineDataPromise, metaDataPromise, geoDataPromise]);
 
-  const container = select('.map');
+  const playButton = select('.play-button')
+    .on('click', handlePlay);;
+  const resetButton = select('.reset-button')
+    .on('click', handleReset);;
+  const dateInput = select('.date-input')
+    .on('input', handleInput);;
   const dateCounterElement = select('.date');
-  const sliderElement = select('.date-input');
-  const playButton = select('.play-button');
-  const resetButton = select('.reset-button');
+
+  const container = select('.map');
 
   const bounds = container.node().getBoundingClientRect();
   const width = bounds.width;
@@ -41,43 +45,106 @@ async function init() {
     .attr('height', height)
     .attr('viewBox', `0 0 ${width} ${height}`);
 
-  const mapGroup = svg.append('g')
+  const map = svg.append('g')
 
-  mapGroup.append('path')
+  map.append('path')
     .attr('d', path(geoData))
     .attr('fill', '#6A6E7F')
     .attr('stroke', '#3A3C49')
     .attr('stroke-width', 1.5);
 
-  for (let data of timelineData) {
+  function update(data, index) {
     const mergedData = data.map(d => {
       const meta = metaData.filter(md => md.ags === d.ags)[0];
       return Object.assign({}, d, meta);
     })
 
-    const circleUpdate = mapGroup.selectAll('circle')
+    const circleUpdate = map.selectAll('circle')
       .data(mergedData, d => d.ags)
-      .attr('fill', 'orange')
       .attr('fill-opacity', 0.75)
+      .on('mouseenter', d => console.log(d))
 
     circleUpdate
       .transition()
       .duration(500)
+      .attr('fill', d => {
+        if (d.valuePer100Tsd >= 50) {
+          return '#f03b20'; // red
+        } else if (d.valuePer100Tsd >= 35) {
+          return '#feb24c'; // orange
+        } else {
+          return '#ffeda0'; // yellow
+        }
+      })
       .attr('r', d => d.valuePer100Tsd ? scale(d.valuePer100Tsd) : 0);
 
-    const circleEnter = circleUpdate
+    circleUpdate
       .enter()
       .append('circle')
-      .attr('fill', 'green')
-      .attr('id', d => `ags-${d.ags}`)
       .attr('cx', d => projection([d.long, d.lat])[0])
       .attr('cy', d => projection([d.long, d.lat])[1]);
 
-    const circleExit = circleUpdate.exit().remove();
-
     dateCounterElement.text(data[0].date);
+    dateInput.property('value', index);
+  }
 
-    await sleep(500)
+  const animation = animationControl();
+  animation.start();
+
+  function handlePlay() {
+    if (playButton.classed('playing')) {
+      playButton.classed('playing', false);
+      animation.stop();
+    } else {
+      playButton.classed('playing', true);
+      animation.start();
+    }
+  }
+
+  function handleReset() {
+    animation.stop();
+    setTimeout(() => {
+      animation.set(0);
+      animation.start();
+    }, 500);
+  }
+
+  function handleInput() {
+    const value = parseInt(dateInput.property('value'));
+    update(timelineData[value], value);
+    animation.stop();
+    animation.set(value);
+    playButton.classed('playing', false);
+  }
+
+  function animationControl() {
+    let isPlaying = false;
+    let maxIndex = timelineData.length;
+    let currentIndex = 0;
+
+    function tick(index) {
+      if (index < maxIndex && isPlaying) {
+        update(timelineData[index], index);
+        currentIndex = ++index;
+        // await sleep(500);
+        setTimeout(() => tick(currentIndex), 500);
+      }
+    }
+
+    function set(newIndex) {
+      currentIndex = newIndex;
+    }
+
+    function start() {
+      isPlaying = true;
+      tick(currentIndex);
+    }
+
+    function stop() {
+      isPlaying = false;
+    }
+
+    return { set, start, stop }
   }
 
   // handleUpdate();
